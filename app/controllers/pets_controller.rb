9e9484a26e5986ee69809188f2bf7fb57c3d8334
Marcos Suarez <marcossuarezr88@gmail.com>
2022-12-29 18:20:47 +0100

    my lost kity

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,46 @@
 class PetsController < ApplicationController
-  def home
-    render html: "<h1>Hola, Rails!</h1>".html_safe
+  before_action :set_pet, only: [ :show, :edit, :update, :delete ]
+  def index
+    @pets = Pet.all
+  end
+
+  def show; end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @pet.destroy
+    redirect_to pets_path, status: :see_other
+  end
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :address, :found_on, :species)
   end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,12 @@
 class Pet < ApplicationRecord
+  SPECIES = %w[ dog cat lama hamster kangaroo ]
+
+  validates :species, inclusion: { in: SPECIES }
+  before_validation :downcase_species
+
+  private
+
+  def downcase_species
+    species.downcase!
+  end
 end
